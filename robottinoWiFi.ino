1a8124892ea2b9aee67d91ada24f340efae2d510
Prof-Palitto <94290557+Prof-Palitto@users.noreply.github.com>
2022-11-02 12:34:17 +0100

    Add files via upload

---
+++ robottinoWiFi.ino
@@ -0,0 +1,76 @@
+#include <SoftwareSerial.h>
+
+#define rxPin 10
+#define txPin 8
+// Set up a new SoftwareSerial object
+SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+
+String check4answer(){
+    String str = "";
+    while (mySerial.available() > 0) {
+        char c = mySerial.read();
+        Serial.print(c);
+        str += String(c);
+    }
+    return str;
+}
+
+
+String esp01cmd(String cmd) {
+  Serial.println("sending: " + cmd);
+  mySerial.println(cmd);
+  delay(10);
+  String risposta = check4answer();
+  return risposta;
+}
+
+void setup()  {
+    // Define pin modes for TX and RX
+    // pinMode(rxPin, INPUT);
+    // pinMode(txPin, OUTPUT);
+    
+    // Set the baud rate for the SoftwareSerial object
+    mySerial.begin(115200);
+    Serial.begin(9600);
+    delay(1000);
+    esp01cmd("AT");
+    delay(1000);
+    esp01cmd("AT+CWMODE=2");
+    delay(1000);
+    esp01cmd("AT+CWSAP=\"robottino\",\"robottino\",1,4");
+    delay(1000);
+    esp01cmd("AT+CIFSR"); //show AP IP address
+    esp01cmd("AT+CIPMUX=1"); //allow up to 1 connections at the time
+    
+    
+    Serial.println("ESP-01 Configuration Completed");
+}
+
+void loop() {
+    Serial.println("loop...");
+    while(esp01cmd("AT+CWLIF").substring(11,18) != "192.168") {
+      Serial.println("no connections so far... still waiting");
+      delay(1000);
+    }
+
+    Serial.println("Connection from remote device was Established!!!");
+    //Socket ID: 3
+    //accept packets from any IP address/devices
+    //Listen to local port 4567
+    //outgoing packets could go to any remote host without restrictions...
+    esp01cmd("AT+CIPSTART=3,\"UDP\",\"0.0.0.0\",0,4567,2"); //starting UDP Socket Server 
+    
+//    String str = ;    
+//    Serial.println("received: "+esp01cmd("AT+CWLIF").substring(11,18));
+    delay(3000);
+    while(true) {
+      String str = mySerial.readString();
+      if(str != "") {
+        int startOfSTR = str.indexOf(":",10)+1;
+        Serial.println("Received: "+str.substring(startOfSTR));
+        //Serial.println("Received: "+str);
+        //Serial.println(startOfSTR);
+      }
+    }
+
+}
